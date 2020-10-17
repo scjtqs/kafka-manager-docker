7435c2af31d900027ca5abbdaa8dc9a50121c9c0
scjtqs <scjtqs@qq.com>
2020-10-17 09:52:08 +0800

    dockerfix

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --use --name mybuilder
+docker buildx build --tag scjtqs/kafka-manager:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le,linux/s390x --push .
+docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/kafka-manager:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le,linux/s390x --push .
+docker buildx rm mybuilder
